00fa057294db82029a46962b23dfdfb87dce411e
olakadasami <olakadasamuel@gmail.com>
2022-08-08 17:13:54 +0100

    Styled the Header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
+    <div className="bg-gray-900 text-gray-400">
       <Home />
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,20 +3,23 @@ import Button from './Button'
 
 const Header = () => {
     return (
-        <div className='h-screen px-8 grid place-items-center'>
-            <p className='font'>
+        <div className='h-screen px-8 py-40 '>
+            <p className='mb-8'>
                 Hello, my name is
             </p>
-            <h1>
-                <span>Olakada Samuel.</span>
-                <span>I build web applications.</span>
+            <h1 className='text-4xl te mb-8 md:text-6xl'>
+                <span className='block mb-4 text-gray-200 l'>Olakada Samuel.</span>
+                <span className='block '>I build web applications.</span>
             </h1>
-            <p>
-                I'm a web developer specialized in creating beautiful user interfaces(UI) to give users amazing digital experiences.
+            <p className='text-lg mb-8'>
+                I'm a web developer specialized in creating beautiful user interfaces(UI) to give users amazing digital experience.
             </p>
-            <Button>
-                My Resume
-            </Button>
+
+            <div className=''>
+                <Button>
+                    My Resume
+                </Button>
+            </div>
         </div>
     )
 }
