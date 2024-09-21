03f0e86f8e3fe27559d6635bf7bf62d701a765cd
olakadasami <olakadasamuel@gmail.com>
2024-09-21 15:08:04 +0100

    chore: add poppins font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        trans: "rgba(0, 0, 0, 0.3)"
-      }
+        trans: "rgba(0, 0, 0, 0.3)",
+      },
+      fontFamily: {
+        poppins: "Poppins, sans-serif",
+      },
     },
   },
   plugins: [],
-}
+};
