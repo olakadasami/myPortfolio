b7461bbe3548a35bd8b95a5fb14ada68db163530
olakadasami <olakadasamuel@gmail.com>
2024-09-21 15:11:04 +0100

    restructure: add react query to use strapi cms data

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,50 +1,41 @@
+import { useQuery } from "@tanstack/react-query";
 import WorkItem from "./WorkItem";
 
+type TProject = {
+  id: number;
+  title: string;
+  liveUrl: string;
+  githubUrl: string;
+  technologies: string;
+  description: string;
+  tag: string;
+};
+
+const fetchProjects = async () => {
+  const token = `b2689a49ac239858e047f9e101a498d7f0202284b5258d73ba3423fbfebda0181d29d6b90fd61bdef37f66e92854454e8ee3daf67d7ab821f8240370ee23af2c99afd251e9258cf3b4dbbc96d2d3018398c07e15554a974bf5c8149d568f8f5bf9d1028d73000e4da7c3892db6aa81bf0c29f9e291a4445b2a694d8fa3a452ce`;
+  const res = await fetch("http://localhost:1337/api/projects", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (!res.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return res.json();
+};
+
 const Work = () => {
-  const projects = [
-    {
-      name: "AnonAmebo",
-      technologies: "Adonisjs, TailwindCSS",
-      live: "https://anonamebo.onrender.com",
-      github: "https://github.com/olakadasami/AnonAmebo",
-    },
-    {
-      name: "Crappo",
-      technologies: "React, TailwindCSS",
-      live: "https://crappo-olakadasami.vercel.app/",
-      github: "https://github.com/olakadasami/crappo",
-    },
-    {
-      name: "My Portfolio",
-      technologies: "React, TailwindCSS",
-      live: "https://olakadasamuel.site",
-      github: "https://github.com/olakadasami/myPortfolio",
-    },
-    {
-      name: "allStores e-commerce",
-      technologies: "ReactJs, TailwindCSS",
-      live: "https://olaks-allstores.netlify.app/",
-      github: "https://github.com/olakadasami/allStores",
-    },
-    {
-      name: "Insure Landing",
-      technologies: "HTML5, CSS3",
-      live: "https://insurelanding-olaks.netlify.app/",
-      github: "https://github.com/olakadasami/insure",
-    },
-    {
-      name: "HomeSmart Furniture",
-      technologies: "SCSS, HTML5",
-      live: "https://olaks-furniture-rental.netlify.app/",
-      github: "https://github.com/olakadasami/furniture",
-    },
-    {
-      name: "Intro Section",
-      technologies: "CSS3, HTML5",
-      live: "https://fem-intro-section-olaks.netlify.app/",
-      github: "https://github.com/olakadasami/intro-section-FEM",
-    },
-  ];
+  const {
+    data: projects,
+    error,
+    isLoading,
+  } = useQuery({ queryKey: ["projects"], queryFn: fetchProjects });
+
+  console.log({ projects });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error instanceof Error) return <div>Error: {error.message}</div>;
+
   return (
     <section id="work" className="px-12 py-20 ">
       <h2 className="text-4xl font-bold text-white mb-12">
@@ -54,15 +45,17 @@ const Work = () => {
         My Projects
       </h2>
 
-      <div className="flex gap-20 flex-wrap justify-center">
-        {projects.map((project, index) => (
+      <div className="flex flex-col gap-10">
+        {projects?.data?.map((project: TProject, index: number) => (
           <WorkItem
-            key={index}
-            title={project.name}
-            live={project.live}
-            image={project.image}
-            github={project.github}
+            key={project.id}
+            title={project.title}
+            live={project.liveUrl}
+            github={project.githubUrl}
             technologies={project.technologies}
+            description={project.description}
+            tag={project.tag}
+            index={index}
           />
         ))}
       </div>
